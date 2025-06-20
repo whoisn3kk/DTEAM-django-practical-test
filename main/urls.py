ef25c73ad11eee8e575be4ef719be45e120b3a7d
whoisn3kk <dmytrenkodaniel@gmail.com>
2025-06-20 18:06:42 +0300

    +pdf

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('', CVListView.as_view(), name='cv_list'),
     path('cv/<int:pk>/', CVDetailView.as_view(), name='cv_detail'),
+    path('cv/<int:pk>/pdf/', gen_cv_pdf, name='cv_pdf'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import *
+from django.http import HttpResponse, HttpRequest
+from django.template.loader import get_template
+from io import BytesIO
+import pdfkit
 
 # Create your views here.
 
@@ -18,3 +22,16 @@ class CVDetailView(DetailView):
 
     def get_queryset(self):
         return super().get_queryset().prefetch_related('skills', 'projects')
+
+def gen_cv_pdf(request:HttpRequest, pk:int):
+    cv = CV.objects.get(pk=pk)
+    template = get_template('main/cv_pdf_template.html')
+    html = template.render({'cv': cv})
+
+
+    buffer = BytesIO(pdfkit.from_string(html)) # type: ignore
+    buffer.seek(0)
+    response = HttpResponse(buffer.getvalue(), content_type='application/pdf')
+    response['Content-Disposition'] = f'attachment; filename="CV_{cv.pk}.pdf"'
+
+    return response
