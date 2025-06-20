d636ddab4f79ec00329158d82fdd515e5c7e2bdc
whoisn3kk <dmytrenkodaniel@gmail.com>
2025-06-20 15:58:52 +0300

    list, detail CV views + tests

--- main/views.py
+++ main/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import *
 
 # Create your views here.
+
+class CVListView(ListView):
+    model = CV
+    template_name = 'main/cv_list.html'
+    context_object_name = 'cv_list'
+
+    def get_queryset(self):
+        return CV.objects.prefetch_related('skills').all() 
+
+class CVDetailView(DetailView):
+    model = CV
+    template_name = 'main/cv_detail.html'
+
+    def get_queryset(self):
+        return super().get_queryset().prefetch_related('skills', 'projects')
